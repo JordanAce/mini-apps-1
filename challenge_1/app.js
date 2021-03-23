1b4ba748f73712411ce8d4278d0070a966295b8a
Jordan <jordanace83@gmail.com>
2021-03-23 17:14:04 -0500

    Alternate Between X and O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,112 @@
+  var numClicks = 0;
 
+  handleClickOne = function()  {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+  var text = document.createTextNode("X");
+  } else {
+    var text = document.createTextNode("O");
+  }
+  node.appendChild(text);
+  document.getElementById("squareOne").appendChild(node);
+  console.log('CLICKED');
+}
+
+
+var handleClickTwo = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareTwo").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickThree = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareThree").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickFour = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareFour").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickFive = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareFive").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickSix = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareSix").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickSeven = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareSeven").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickEight = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareEight").appendChild(node);
+  console.log('CLICKED');
+}
+
+var handleClickNine = function() {
+  numClicks++;
+  var node = document.createElement("p");
+  if (numClicks % 2 === 1) {
+    var text = document.createTextNode("X");
+    } else {
+      var text = document.createTextNode("O");
+    }  node.appendChild(text);
+  document.getElementById("squareNine").appendChild(node);
+  console.log('CLICKED');
+}
 
