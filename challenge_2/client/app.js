1462970988cc0ff0ea014272d63ff0982d406561
Jordan <jordanace83@gmail.com>
2021-03-27 18:31:10 -0500

    Working on AJAX refactor

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,11 +1,37 @@
-// let handleSubmit = function(event) {
-//   // event.preventDefault();
-//   // let formData = JSON.stringify(event);
-//   // console.log(formData);
+const csvOutput = document.getElementById('#CSV');
 
-//   // console.log('client side connected');
-// }
+const postHandler = {
+  post: (textarea, callback, err = null) => {
+    event.preventDefault();
+    $.ajax({
+      url: 'http://localhost:3000/',
+      type: 'POST',
+      data: textarea,
+      contentType: 'application/json',
+      dataType: 'json',
+      success: callback,
+      error: function() {
+        console.log('ERROR', err);
+      }
+    });
+  }
+};
 
+const dataConversionSuccesful = function(parsedData) {
+  $(csvOutput).val();
+  $(csvOutput).val(parsedData);
+}
 
 // eslint-disable-next-line max-len
-// document.querySelector('input[type = "submit"]').addEventListener("click", handleSubmit);
+$("form").on('submit', function(event) {
+  event.preventDefault();
+  let txt = $('textarea').val();
+  postHandler.post(txt, dataConversionSuccesful);
+  console.log('ON SUBMIT WORKS');
+})
+// document.querySelector('input[type = "submit"]').addEventListener("click", function() {
+//   let txt = $('textarea').val();
+//   postHandler.post(txt, dataConversionSuccesful);
+// });
+
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -9,21 +9,19 @@ const app = express();
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended": true}));
-
-app.get('/', function (req, res) {
-res.render('index.html');
-res.end();
-})
-
-app.post('/upload_json', (req, res) => {
-let data = req.body.json.split(',');
-let parsedData = JSON.parse(data);
-console.log(typeof (parsedData));
-res.send(parsedData.children);
-res.end();
-});
-
-
 app.listen(port, () => {
   console.log(`Sever listening at http://localhost:${port}`);
 });
+
+app.get('/', function (req, res) {
+res.render('index');
+})
+
+app.post('/', (req, res) => {
+let data = req.body.json.split(',');
+let parsedData = JSON.parse(data);
+res.send(parsedData);
+res.end();
+});
+
+
