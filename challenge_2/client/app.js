f6fd3b7996e6ac1bc29ac7f6ef6d6df00b2b06f9
Jordan <jordanace83@gmail.com>
2021-03-28 16:08:08 -0500

    CSV Generator working with submit button

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,9 +1,9 @@
-const csvOutput = document.querySelector('#CSV');
+const csvOutput = document.querySelector('textarea#csv');
 
 // eslint-disable-next-line max-len
 $("form").on('submit', function(event) {
   event.preventDefault();
-  console.log(csvOutput);
+  $(csvOutput).empty();
   let txt = $('textarea').val();
   $.ajax({
     url: 'http://localhost:3000/',
@@ -12,14 +12,12 @@ $("form").on('submit', function(event) {
     contentType: 'application/json',
     dataType: 'json',
     success: function (response) {
-      console.log(response);
-      csvOutput.append(response);
+      $(csvOutput).append(response);
     },
     error: function(err) {
       console.log('ERROR', err);
     }
   });
-  console.log('ON SUBMIT WORKS');
 })
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -16,9 +16,35 @@ app.listen(port, () => {
 app.get('/', function (req, res) {
 res.render('index');
 })
+const convertToCSV = function (jsonData) {
+  let csvString = '';
+  let items = Object.keys(jsonData).splice(0, Object.keys(jsonData).length - 1);
+  for (let i = 0; i < items.length; i++) {
+    if (i === items.length - 1) {
+      csvString += items[i] + '\n';
+    } else {
+      csvString += items[i] + ', ';
+    }
+  }
 
+  let innerfunction = function(jsonData) {
+    for (var key in jsonData) {
+      if (key === 'sales') {
+        csvString += jsonData[key] + '\n';
+      } else if (key !== 'children') {
+        csvString += jsonData[key] + ', ';
+      } else if (jsonData.children) {
+        for (let j = 0; j < jsonData.children.length; j++) {
+          innerfunction(jsonData.children[j]);
+        }
+      }
+    }
+  }
+  innerfunction(jsonData);
+  return csvString;
+}
 app.post('/', (req, res) => {
-  let data = JSON.stringify(req.body);
+  let data = convertToCSV(req.body);
   res.send(JSON.stringify(data));
   res.end();
 });
