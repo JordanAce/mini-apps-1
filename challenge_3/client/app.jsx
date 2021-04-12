e673a5f6562f4d7cdb005fb026ee908172e5a63b
Jordan <jordanace83@gmail.com>
2021-04-11 20:36:56 -0500

    ReactJS with Express working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,41 @@
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form : ''
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>
+TESTING AGAIN        </h2>
+      </div>
+      )
+  }
+}
+const TestList = function() {
+  return(
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+const ThisRepo = function() {
+  return(
+  <div>THIS REPO IS STUPID</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<TestList />, document.getElementById("testlist"))
+ReactDOM.render(<ThisRepo />, document.getElementById("isstupid"))
+
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const port = 3000;
+let app = express();
 
 
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`)
+});
