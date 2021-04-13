f6b4cb3f83ad5f24ce6c353ba223d7f89a3396fb
Jordan <jordanace83@gmail.com>
2021-04-12 22:19:43 -0500

    Working on prop flow

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -3,30 +3,66 @@ class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      information : false
+      information : false,
+      address: false,
+      ccinfo: false,
+      confirmation: false
     }
     this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
+    this.handleInformationClick = this.handleInformationClick.bind(this);
+    this.handleAddressClick = this.handleAddressClick.bind(this);
+    this.handleCCInfoClick = this.handleCCInfoClick.bind(this);
+    this.handleConfirmationClick = this.handleConfirmationClick.bind(this);
   }
 
+  //Click handlers
   handleCheckoutClick() {
     this.setState ({
       information: true
     })
   }
 
+  handleInformationClick() {
+    this.setState({
+      address: true
+    })
+  }
 
+  handleAddressClick() {
+    this.setState({
+      ccinfo: true
+    })
+  }
+
+  handleCCInfoClick() {
+    this.setState({
+      confirmation: true
+    })
+  }
+
+  handleConfirmationClick() {
+    this.setState({
+      information : false,
+      address: false,
+      ccinfo: false,
+      confirmation: false
+    })
+  }
 
   render() {
     const proceed = this.state.information;
     let button;
     if (!proceed) {
       button = <button onClick = {this.handleCheckoutClick.bind(this)} />
+    } else {
+      return(
+        <Information proceed = {this.state.information} address = {this.state.address} ccinfo = {this.state.ccinfo} confirmation = {this.state.confirmation} handleInformationClick = {this.handleInformationClick} handleAddressClick = {this.handleAddressClick}/>
+      )
     }
 
     return (
       <div>
         <h2>
-         <Information proceed = {proceed} />
          {button}
         </h2>
       </div>
@@ -34,26 +70,45 @@ class HomePage extends React.Component {
   }
 }
 
-const Information = function() {
+
+const Information = function(props) {
+    let button;
+    if (props.proceed) {
+      button = <button onClick = {props.handleInformationClick.bind(this)} />
+    }
   return(
     <div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
+      INFORMATION FORMS
+      {button}
     </div>
   )
 }
 
-const ThisRepo = function() {
+const Address = function() {
   return(
-  <div>THIS REPO IS STUPID</div>
+  <div>ADDRESS FORMS</div>
   )
 }
 
+const CCInfo = function() {
+  return(
+  <div>CCInfo FORMS</div>
+  )
+}
+
+const Confirmation = function() {
+  return(
+  <div>Confirmation PAGE</div>
+  )
+}
+
+
 ReactDOM.render(<HomePage />, document.getElementById("homepage"))
-ReactDOM.render(<Information />, document.getElementById("information"))
-ReactDOM.render(<ThisRepo />, document.getElementById("isstupid"))
+// ReactDOM.render(<Information />, document.getElementById("information"))
+// ReactDOM.render(<Address />, document.getElementById("address"))
+// ReactDOM.render(<CCInfo />, document.getElementById("ccinfo"))
+// ReactDOM.render(<Confirmation />, document.getElementById("confirmation"))
+
+
 
 
