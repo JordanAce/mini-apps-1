1a1f08a84a3b9d4efb57c304fe5d06983debd619
Jordan <jordanace83@gmail.com>
2021-04-13 23:05:05 -0500

    Working on input field data retrieval

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,4 +1,5 @@
 
+
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
@@ -6,26 +7,47 @@ class HomePage extends React.Component {
       information : false,
       address: false,
       ccinfo: false,
-      confirmation: false
+      confirmation: false,
+      value : ''
+
     }
     this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
     this.handleInformationClick = this.handleInformationClick.bind(this);
     this.handleAddressClick = this.handleAddressClick.bind(this);
     this.handleCCInfoClick = this.handleCCInfoClick.bind(this);
     this.handleConfirmationClick = this.handleConfirmationClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   //Click handlers
+  handleChange(event) {
+    this.setState({value:event.target.value})
+  }
+
   handleCheckoutClick() {
     this.setState ({
       information: true
     })
   }
 
-  handleInformationClick() {
+  handleInformationClick(event) {
     this.setState({
       address: true
     })
+
+    console.log(event)
+    $.ajax({
+      type: 'POST',
+      url: ('/ccinfo'),
+      data: event.target.value,
+      success: function (data) {
+        console.log('SUCCESS POST CLICK')
+      },
+      error: function (error) {
+        console.log('ERROR POST CLICK')
+      }
+    })
+    event.preventDefault();
   }
 
   handleAddressClick() {
@@ -52,49 +74,58 @@ class HomePage extends React.Component {
     )
   }
 
+  //Homepage renders Information on Checkout click
   render() {
     const proceed = this.state.information;
     let button;
     if (!proceed) {
-      button = <button onClick = {this.handleCheckoutClick.bind(this)} />
+      button = <button onClick = {this.handleCheckoutClick.bind(this)}>Checkout</button>
     } else {
       return(
-        <Information proceed = {this.state.information} address = {this.state.address} ccinfo = {this.state.ccinfo} confirmation = {this.state.confirmation} handleInformationClick = {this.handleInformationClick} handleAddressClick = {this.handleAddressClick} handleCCInfoClick = {this.handleCCInfoClick} handleConfirmationClick ={this.handleConfirmationClick}/>
+        <Information proceed = {this.state.information} address = {this.state.address} ccinfo = {this.state.ccinfo} confirmation = {this.state.confirmation} handleInformationClick = {this.handleInformationClick} handleAddressClick = {this.handleAddressClick} handleCCInfoClick = {this.handleCCInfoClick} handleConfirmationClick ={this.handleConfirmationClick} handleChange = {this.handleChange}/>
       )
     }
 
     return (
       <div>
-        <h2>
-         {button}
-        </h2>
+        <h1>
+          Let's Buy Something!
+        </h1><br></br>
+        {button}
       </div>
     )
   }
 }
 
-
+//Enter information then render Address on Next click
 const Information = function(props) {
     let button;
+
     if(!props.address) {
-      button = <button onClick = {props.handleInformationClick.bind(this)} />
+      return(
+        <div>
+          <h1>
+          Enter Your Information:
+          </h1><br></br>
+          <form onSubmit = {props.handleInformationClick}>
+          Full Name: &nbsp; <input type = "text" id = "name"></input><br></br>
+          E-Mail: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "text" id = "email"></input><br></br>
+          Password: &nbsp;&nbsp;  <input type = "text" id = "password"></input><br></br>
+          <input type ="submit" value ="Next" onChange = {props.handleChange}></input>
+          </form>
+        </div>
+      )
     } else {
       return (
         <Address proceed = {props.information} address = {props.address} ccinfo = {props.ccinfo} confirmation = {props.confirmation} handleInformationClick = {props.handleInformationClick} handleAddressClick = {props.handleAddressClick} handleCCInfoClick = {props.handleCCInfoClick} handleConfirmationClick = {props.handleConfirmationClick}/>
       )
     }
-  return(
-    <div>
-      INFORMATION FORMS
-      {button}
-    </div>
-  )
 }
-
+//Enter Address then render CC info on Next click
 const Address = function(props) {
   let button;
   if(!props.ccinfo) {
-    button = <button onClick = {props.handleAddressClick.bind(this)} />
+    button = <button onClick = {props.handleAddressClick.bind(this)}>Next</button>
   } else {
     return (
       <CCInfo proceed = {props.information} address = {props.address} ccinfo = {props.ccinfo} confirmation = {props.confirmation} handleInformationClick = {props.handleInformationClick} handleAddressClick = {props.handleAddressClick} handleCCInfoClick = {props.handleCCInfoClick} handleConfirmationClick ={props.handleConfirmationClick}/>
@@ -102,16 +133,24 @@ const Address = function(props) {
   }
     return(
       <div>
-        ADDRESS FORMS
-        {button}
-      </div>
+      <h1>
+      Enter Your Address:
+      </h1><br></br>
+      Line 1: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "text" id = "ln1"></input><br></br>
+      Line 2: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "text" id = "ln2"></input><br></br>
+      City: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "text" id = "city"></input><br></br>
+      State: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type = "text" id = "state"></input><br></br>
+      Zip Code: &nbsp;&nbsp; <input type = "text" id = "zip"></input><br></br>
+      Phone #: &nbsp;&nbsp;&nbsp;&nbsp; <input type = "text" id = "phonenum"></input><br></br>
+      {button}
+    </div>
 )
 }
-
+//Enter CC information then render Confirmation page on Next
 const CCInfo = function(props) {
   let button;
   if(!props.confirmation) {
-    button = <button onClick = {props.handleCCInfoClick.bind(this)} />
+    button = <button onClick = {props.handleCCInfoClick.bind(this)}>Next</button>
   } else {
     return (
       <Confirmation proceed = {props.information} address = {props.address} ccinfo = {props.ccinfo} confirmation = {props.confirmation} handleInformationClick = {props.handleInformationClick} handleAddressClick = {props.handleAddressClick} handleCCInfoClick = {props.handleCCInfoClick} handleConfirmationClick = {props.handleConfirmationClick}/>
@@ -119,18 +158,26 @@ const CCInfo = function(props) {
   }
 return(
   <div>
-    CC Info Forms
-    {button}
-  </div>
+      <h1>
+      Enter Your Credit Card Information:
+      </h1><br></br>
+      Card Number: &nbsp;&nbsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp; <input type = "text" id = "ln1"></input><br></br>
+      Expiration MM/DD/YYYY: <input type = "text" id = "ln2"></input><br></br>
+      CVV: &nbsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp; <input type = "text" id = "city"></input><br></br>
+      Billing ZIP: &nbsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; <input type = "text" id = "state"></input><br></br>
+      {button}
+    </div>
 )
 }
-
+//Show confirmation page with summary of information entered, then render HomePage on Purchase click
 const Confirmation = function(props) {
   let button;
-  button = <button onClick = {props.handleConfirmationClick.bind(this)} />
+  button = <button onClick = {props.handleConfirmationClick.bind(this)}>Purchase</button>
   return(
     <div>
-      Confirmation Page
+      <h1>
+      Confirm your Information:
+      </h1><br></br>
       {button}
     </div>
   )
@@ -138,10 +185,6 @@ const Confirmation = function(props) {
 
 
 ReactDOM.render(<HomePage />, document.getElementById("homepage"))
-// ReactDOM.render(<Information />, document.getElementById("information"))
-// ReactDOM.render(<Address />, document.getElementById("address"))
-// ReactDOM.render(<CCInfo />, document.getElementById("ccinfo"))
-// ReactDOM.render(<Confirmation />, document.getElementById("confirmation"))
 
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const port = 3000;
 let app = express();
+const bodyParser = require('body-parser')
+const port = 3000;
+const db = require('./database/index.js')
 
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/ccinfo', function(req,res) {
+  console.log('INSIDE POST', req)
+})
+
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`)
