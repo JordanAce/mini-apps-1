109b75bafbb53c4362e69bc5ad2d5e6d2d79f3d1
Jordan <jordanace83@gmail.com>
2021-04-22 18:24:17 -0500

    Targeting correct cells

--- challenge_4/src/components/ConnectFourBoard.jsx
+++ challenge_4/src/components/ConnectFourBoard.jsx
@@ -6,66 +6,71 @@ class ConnectFourBoard extends React.Component {
     this.state = {
       playerTurn : 1
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    console.log(event.target.id[0])
   }
 
   render () {
     return (
-    <table style = {{width:'75%'}}>
-      <caption>CONNECT 4</caption><br></br>
+    <table style = {{width:'75%'}} className="ConnectFourBoard" onClick={this.props.onClick}>
+      <caption>CONNECT 4</caption>
       <tbody>
       <tr>
-        <td id = "6:1"></td>
-        <td id = "6:2"></td>
-        <td id = "6:3"></td>
-        <td id = "6:4"></td>
-        <td id = "6:5"></td>
-        <td id = "6:6"></td>
-        <td id = "6:7"></td>
+        <td id = "1:6" onClick = {this.handleClick}></td>
+        <td id = "2:6" onClick = {this.handleClick}></td>
+        <td id = "3:6" onClick = {this.handleClick}></td>
+        <td id = "4:6" onClick = {this.handleClick}></td>
+        <td id = "5:6" onClick = {this.handleClick}></td>
+        <td id = "6:6" onClick = {this.handleClick}></td>
+        <td id = "7:6" onClick = {this.handleClick}></td>
       </tr>
       <tr>
-        <td id = "5:1"></td>
-        <td id = "5:2"></td>
-        <td id = "5:3"></td>
-        <td id = "5:4"></td>
-        <td id = "5:5"></td>
-        <td id = "5:6"></td>
-        <td id = "5:7"></td>
+        <td id = "1:5" onClick = {this.handleClick}></td>
+        <td id = "2:5" onClick = {this.handleClick}></td>
+        <td id = "3:5" onClick = {this.handleClick}></td>
+        <td id = "4:5" onClick = {this.handleClick}></td>
+        <td id = "5:5" onClick = {this.handleClick}></td>
+        <td id = "6:5" onClick = {this.handleClick}></td>
+        <td id = "7:5" onClick = {this.handleClick}></td>
       </tr>
       <tr>
-        <td id = "4:1"></td>
-        <td id = "4:2"></td>
-        <td id = "4:3"></td>
-        <td id = "4:4"></td>
-        <td id = "4:5"></td>
-        <td id = "4:6"></td>
-        <td id = "4:7"></td>
+        <td id = "1:4" onClick = {this.handleClick}></td>
+        <td id = "2:4" onClick = {this.handleClick}></td>
+        <td id = "3:4" onClick = {this.handleClick}></td>
+        <td id = "4:4" onClick = {this.handleClick}></td>
+        <td id = "5:4" onClick = {this.handleClick}></td>
+        <td id = "6:4" onClick = {this.handleClick}></td>
+        <td id = "7:4" onClick = {this.handleClick}></td>
       </tr>
       <tr>
-        <td id = "3:1"></td>
-        <td id = "3:2"></td>
-        <td id = "3:3"></td>
-        <td id = "3:4"></td>
-        <td id = "3:5"></td>
-        <td id = "3:6"></td>
-        <td id = "3:7"></td>
+        <td id = "1:3" onClick = {this.handleClick}></td>
+        <td id = "2:3" onClick = {this.handleClick}></td>
+        <td id = "3:3" onClick = {this.handleClick}></td>
+        <td id = "4:3" onClick = {this.handleClick}></td>
+        <td id = "5:3" onClick = {this.handleClick}></td>
+        <td id = "6:3" onClick = {this.handleClick}></td>
+        <td id = "7:3" onClick = {this.handleClick}></td>
       </tr>
       <tr>
-        <td id = "2:1"></td>
-        <td id = "2:2"></td>
-        <td id = "2:3"></td>
-        <td id = "2:4"></td>
-        <td id = "2:5"></td>
-        <td id = "2:6"></td>
-        <td id = "2:7"></td>
+        <td id = "1:2" onClick = {this.handleClick}></td>
+        <td id = "2:2" onClick = {this.handleClick}></td>
+        <td id = "3:2" onClick = {this.handleClick}></td>
+        <td id = "4:2" onClick = {this.handleClick}></td>
+        <td id = "5:2" onClick = {this.handleClick}></td>
+        <td id = "6:2" onClick = {this.handleClick}></td>
+        <td id = "7:2" onClick = {this.handleClick}></td>
       </tr>
       <tr>
-        <td id = "1:1"></td>
-        <td id = "1:2"></td>
-        <td id = "1:3"></td>
-        <td id = "1:4"></td>
-        <td id = "1:5"></td>
-        <td id = "1:6"></td>
-        <td id = "1:7"></td>
+        <td id = "1:1" onClick = {this.handleClick}></td>
+        <td id = "2:1" onClick = {this.handleClick}></td>
+        <td id = "3:1" onClick = {this.handleClick}></td>
+        <td id = "4:1" onClick = {this.handleClick}></td>
+        <td id = "5:1" onClick = {this.handleClick}></td>
+        <td id = "6:1" onClick = {this.handleClick}></td>
+        <td id = "7:1" onClick = {this.handleClick}></td>
       </tr>
       </tbody>
     </table>
